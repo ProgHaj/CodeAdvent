4d505f58784c23be7a8b47572f6e0745fd2f4e15
Haj <flummeltrollet@gmail.com>
2015-12-05 16:44:11 +0000

    added 1.b

--- src/code_xmas/core.clj
+++ src/code_xmas/core.clj
@@ -1,13 +1,24 @@
 (ns code-xmas.core
   (:require [clojure.string :as str]))
 
-(def file (str/split (slurp "resources/brackets") #"\s"))
+(def file (slurp "resources/brackets"))
 
 (defn bracket-left? [char]
   (if (= char \()
     1
     -1))
 
-(apply + (apply map bracket-left? file))
+(apply + (apply map bracket-left? (str/split file #"/s")))
 
 ;part 2
+
+(defn position-basement [file]
+  (loop [floor 0
+         coll file
+         pos 0]
+    (if (< floor 0)
+      pos
+      (recur (+ floor (bracket-left? (first coll))) (rest coll) (inc pos)))))
+
+(position-basement file)
+
